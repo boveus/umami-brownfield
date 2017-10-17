8098bb76ce0531dd31d8c812699a356d96044ca8
samuelssnider <sam_redins@yahoo.com>
2017-10-17 12:19:04 -0600

    working on google test, weird routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
  delete "/cart"        => "carts#destroy"
  get "/:name"          => "tags#show"
 
+ get '/auth/google_oauth2', as: :google_login
  get 'auth/:provider/callback', to: 'sessions#create'
  get 'auth/failure', to: redirect('/')
  get 'signout', to: 'sessions#destroy', as: 'signout'
