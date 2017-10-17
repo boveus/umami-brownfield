6e890328302f27476bfa9c8da1374adc3443831c
erose357 <erose357@gmail.com>
2017-10-16 19:50:12 -0600

    Adds shoulda-matchers tests for the tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:item_tags) }
+  it { is_expected.to have_many(:items) }
+
+  it { is_expected.to have_db_column(:name) }
+  it { is_expected.to have_db_column(:created_at) }
+  it { is_expected.to have_db_column(:updated_at) }
 end
