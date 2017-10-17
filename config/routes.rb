d832eb0e2c283467dc2c87a1edbaae3d2d2ba13d
Brandon Stewart <boveus@gmail.com>
2017-10-17 17:16:38 -0600

    Add initial passing test for vendors index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root to: "items#index"
+ root to: "vendors#index"
+
+ resources :vendors, only: [:index, :show]
 
  resources :items,  only: [:index, :show]
  resources :tags,   only: [:index, :show]
