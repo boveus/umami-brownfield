64bde4d331a9dfb7f642cb666c42620ab6f2d6b7
erose357 <erose357@gmail.com>
2017-10-17 20:56:35 -0600

    Adds test for guest customer can see businesses

---
+++ spec/features/users/guest_customer_can_see_businesses_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "A a guest user(customer)" do
+  scenario "sees photos and names of businesses" do
+    vendors = create_list(:vendor, 3)
+    
+    visit '/'
+   
+    expect(page).to have_content(vendors[0].name)
+    expect(page).to have_content(vendors[1].name)
+    expect(page).to have_content(vendors[2].name)
+
+  #verify whether or not there will be another link and test for that as well
+
+    click_on vendors[1].photo #need to figure out how to find the pic
+
+    expect(path).to eq("/#{vendors[1].name}/items") #need to figure out url
+
+    expect(page).to have_content(vendor[1].name)
+    expect(page).to have_content(vendor[1].photo)
+    expect(page).to have_content(vendor[1].description)
+  end
+end
