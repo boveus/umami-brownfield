637410d7e52cc712e6a57f82afadcca2f23c9169
JoelLindow <joellindow@gmail.com>
2017-10-18 18:15:01 -0600

    removed comments from test file

--- spec/features/users/registered_users/registered_user_can_add_items_from_multiple_vendors_spec.rb
+++ spec/features/users/registered_users/registered_user_can_add_items_from_multiple_vendors_spec.rb
@@ -11,41 +11,22 @@ feature "As an registered user" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
-    # As a registered customer
-    # When I visit the root path
     visit '/'
-
-    # And I click on a business
     click_on "Generic Vendor 1"
-    # And I add items to my cart
-
     click_on("add_shopping_cart")
 
-    # And I visit the root path again
     visit '/'
-
-    # And I visit a different business
     click_on "Generic Vendor 2"
-
-    # And I add items to my cart
     click_on("add_shopping_cart")
 
-    # And I click on the Cart Icon in the navbar
     click_on("shopping_cart")
-
-    # And I click checkout
     click_on("Checkout")
 
-    # I see the order has been created
     expect(page).to have_css(".order", count: 1)
 
-    # I see a list of the items that were purchased
     click_on(class: 'order_date')
 
-    # I see order status as ordered
     expect(page).to have_content("Order Status: ordered")
-
-    # I see the order total
     expect(page).to have_content("$0.05")
     expect(page).to have_content("Order Total: $0.10")
 
