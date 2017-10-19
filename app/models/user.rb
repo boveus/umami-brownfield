e19fe525d52c528d37698e0fcb9523ccfb39824c
samuelssnider <sam_redins@yahoo.com>
2017-10-19 11:59:56 -0600

    all three tests working

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ApplicationRecord
       user.oauth_token = auth["credentials"]["token"]
       user.oauth_expires_at = Time.at(auth["credentials"]["expires_at"]) if auth["credentials"]["expires_at"]
       user.save
-      binding.pry
     end
   end
 end
