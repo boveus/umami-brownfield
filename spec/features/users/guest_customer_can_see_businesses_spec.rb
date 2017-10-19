0c7c542ca75151163c6680fabbad194563f0c143
erose357 <erose357@gmail.com>
2017-10-18 20:47:52 -0600

    Adds test for guest user checks out with items from multiple businesses

--- spec/features/users/guest_customer_can_see_businesses_spec.rb
+++ spec/features/users/guest_customer_can_see_businesses_spec.rb
@@ -20,6 +20,32 @@ RSpec.feature "A a guest user(customer)" do
   end
 
   scenario "can add items from multiple businesses into a single cart" do
+    vendors = create_list(:vendor, 3)
+    items1 = create_list(:item, 3, vendor_id: vendors[0].id)
+    items2 = create_list(:item, 3, vendor_id: vendors[1].id)
+    items3 = create_list(:item, 3, vendor_id: vendors[2].id)
 
+    visit '/'
+
+    click_on vendors[0].name.titleize
+    click_on("add_shopping_cart", match: :first)
+
+    visit '/'
+
+    click_on vendors[1].name.titleize
+    click_on("add_shopping_cart", match: :first)
+
+    visit '/'
+
+    click_on vendors[2].name.titleize
+    click_on("add_shopping_cart", match: :first)
+
+    expect(page).to have_css(".order", count: 1)
+
+    click_on(class: 'order_date')
+    
+    expect(page).to have_content("Order Status: ordered")
+    expect(page).to have_content("0.05")
+    expect(page).to have_content("Order Total: $0.15")
   end
 end
