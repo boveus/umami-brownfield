f8c685d811c9bb9ae2c60c3643162e9ef7e308c9
erose357 <erose357@gmail.com>
2017-10-23 14:16:41 -0600

    Adds expectations to vendor endpoint test

--- spec/requests/api/v1/vendors_request_spec.rb
+++ spec/requests/api/v1/vendors_request_spec.rb
@@ -10,6 +10,9 @@ describe "Vendors API" do
     vendors = JSON.parse(response.body)
     expect(vendors.count).to eq(5)
     expect(vendors.class).to eq(Array)
+    expect(vendors[1]["created_at"]).to be_nil
+    expect(vendors[0]["updated_at"]).to be_nil
+    expect(vendors[2]["image"]).to be_nil
   end
 end
 
