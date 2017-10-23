ce2de5067878b1b552feda9d5c6bad839a3f53fa
JoelLindow <joellindow@gmail.com>
2017-10-23 14:29:42 -0600

    test skeleton set up

---
+++ spec/features/business_manager/business_manager_can_create_new_item_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+feature "business manager logs in" do
+  it "and they add a new item" do
+    user = create(:user)
+    user.roles << Role.create(name: "business_manager", permission_level: 3)
+    user_vendor = create(:vendor)
+    user.update(vendor: user_vendor)
+    vendor = create_list(:vendor, 5)
+    user_vendor.items << create_list(:item, 5)
+    item_name = user_vendor.items.first.name
+
+    visit root_path
+
+    click_on("Login")
+    fill_in "user[name]", with: user.name
+    fill_in "user[password]", with: "Password"
+    click_on("Log in")
+
+    click_on "Generic_Vendor_1"
+
+    click_on "View Items"
+
+    click_on "Add New Item"
+    # I should be taken to a page to fill create a new item with attributes for my business
+
+    expect(current_path).to_be "#{user_vendor.slug}/items/new"
+
+    # After I fill in the item's attributes
+    fill_in "item[name]", with: "New_name"
+    fill_in "item[price]", with: "3.50"
+    fill_in "item[description]", with: "This item costs three fifty"
+
+    # And I click on 'submit'
+    click_on "Create Item"
+
+    # I should be taken to the item's show page
+    expect(current_path).to eq(item_path(user_vendor.items.last))
+
+    # And the item should be added to my main page
+    expect(page).to have_content("New_name")
+    expect(page).to have_content("3.50")
+    expect(page).to have_content("This item costs three fifty")
+
+    #And when I visit user dashboard I see the item as well
+    visit dashboard_path
+
+    expect(page).to have_content("New_name")
+  end
+end
