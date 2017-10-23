6444fac456f4b0a8d957d07f3efb46bf558161b2
erose357 <erose357@gmail.com>
2017-10-23 08:28:56 -0600

    Merchant endpoint is serving json, needs serializer

--- app/controllers/api/v1/vendors_controller.rb
+++ app/controllers/api/v1/vendors_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class VendorsController < Api::V1::ApplicationController
       def index
+        render json: Vendor.all
       end
     end
   end
--- spec/requests/api/v1/vendors_request_spec.rb
+++ spec/requests/api/v1/vendors_request_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-describe "Merchants API" do
+describe "Vendors API" do
   it "sends a list of vendors" do
     create_list(:vendor, 5)
 
     get '/api/v1/vendors.json'
 
     expect(response).to be_success
+    vendors = JSON.parse(response.body)
+    expect(vendors.count).to eq(5)
+    expect(vendors.class).to eq(Array)
   end
 end
 
