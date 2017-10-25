be58912533195f4a0f146c3b827a07b774cdf2bc
erose357 <erose357@gmail.com>
2017-10-24 18:35:33 -0600

    Adds items show endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,4 +18,21 @@ describe "Items API" do
     expect(json[3] ["created_at"]).to be_nil
     expect(json[4] ["updated_at"]).to be_nil
   end
+
+  it "sends one item by id" do
+    items = create_list(:item, 3)
+
+    get "/api/v1/items/#{items[1].id}.json"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json.class).to eq(Hash)
+    expect(json["price"]).to eq((items[1].price.to_f)/100)
+    expect(json["name"]).to eq(items[1].name)
+    expect(json["description"]).to eq(items[1].description)
+    expect(json["status"]).to eq(items[1].status)
+    expect(json["created_at"]).to be_nil
+    expect(json["updated_at"]).to be_nil
+  end
 end
