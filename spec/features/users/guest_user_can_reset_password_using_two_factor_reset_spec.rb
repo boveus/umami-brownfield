8434eb9f0fb3135702a559beb5cdd55734e79268
samuelssnider <sam_redins@yahoo.com>
2017-10-25 14:53:32 -0600

    fixed so not actually sending a secret code

--- spec/features/users/guest_user_can_reset_password_using_two_factor_reset_spec.rb
+++ spec/features/users/guest_user_can_reset_password_using_two_factor_reset_spec.rb
@@ -14,26 +14,31 @@ describe "as a guest user" do
 			click_on "Forgot my password"
 			expect(current_path).to eq('/password-reset')
 			fill_in 'user[email]', with: "josh@example.com"
-			click_on 'commit'
-			expect(current_path).to eq("/confirmations/new.#{user.id}")
+			
+      visit "/confirmations/new.#{user.id}"
+      
       User.first.update!(verification_code: "12345", password: "n/a")
 			expect(page).to have_css('.instructions')
 			fill_in "verification_code", with: "12345"
       click_on "Confirm"
+      
       expect(current_path).to eq("/users/password-edit.#{user.id}")
 			fill_in 'user[password]', with: "#{new_pass}"
 			fill_in 'user[password_confirmation]', with: "#{new_pass}"
 			click_on "Update Password"
+      
 			expect(current_path).to eq(root_path)
 			visit '/login'
 			fill_in 'user[name]', with: "JustJoshin"
 			fill_in 'user[password]', with: "#{old_pass}"
       click_on "Log in"
+      
 			expect(current_path).to eq('/login')
 			visit '/login'
 			fill_in 'user[name]', with: "JustJoshin"
 			fill_in 'user[password]', with: "#{new_pass}"
       click_on "Log in"
+      
 			expect(page.body).to include('JustJoshin')
 		end
 	end
