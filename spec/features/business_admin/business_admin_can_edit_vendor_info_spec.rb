79bca7521053f1d78c48cf8c8ab775519c919b07
JoelLindow <joellindow@gmail.com>
2017-10-26 15:40:35 -0600

    created test file for business admin edit vendor info

---
+++ spec/features/business_admin/business_admin_can_edit_vendor_info_spec.rb
@@ -0,0 +1,5 @@
+# When I log in to my dashboard
+# I should see an index of all of the businesses I am an admin for
+# when I click on the business and view the business' individual page
+# I should see a button to edit the business information for each business
+# when i click on the edit button i should be taken to a page to edit the business inFO
