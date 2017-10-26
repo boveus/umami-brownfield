5851a9e732c215f68809acaf2135161f36c7a098
JoelLindow <joellindow@gmail.com>
2017-10-26 16:45:28 -0600

    create the rest of test content

--- spec/features/business_admin/business_admin_can_edit_vendor_info_spec.rb
+++ spec/features/business_admin/business_admin_can_edit_vendor_info_spec.rb
@@ -9,23 +9,30 @@ feature "business admin logs in" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    # When I log in to my dashboard
     visit '/dashboard'
+
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Business Admin Dashboard")
-    # I should see an index of all of the businesses I am an admin for
     expect(page).to have_content(user_vendor.name)
 
-    # when I click on the business and view the business' individual page
     click_on(user_vendor.name)
 
-    # I should see a button to edit the business information for each business
+    expect(current_path).to eq(vendor_path(user_vendor))
+
     click_on "Edit Vendor Info"
 
-    # when i click on the edit button i should be taken to a page to edit the business inFO
     expect(current_path).to eq(edit_vendor_path(user_vendor))
 
+    fill_in "vendor[name]", with: "Salty Bob's Bitter Stuff Stand"
+    fill_in "vendor[image]", with: "https://usercontent1.hubstatic.com/12507408_f520.jpg"
+    fill_in "vendor[description]", with: "Country Boy Will Survive. Pass me a Busch!"
 
+    click_on "Update"
 
+    expect(current_path).to eq(vendor_path(user_vendor))
+
+    expect(page).to have_content("Salty Bob's Bitter Stuff Stand")
+    expect(page).to have_css("#image")
+    expect(page).to have_content("Country Boy Will Survive. Pass me a Busch!")
   end
 end
